188c657ec28876557aefd684db899d3501f76846
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2023-05-15 16:00:48 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,11 +4,11 @@ public class Main {
   public static void main(String args[])
   {
     Product p=new Product();
-    p.product();
-   p.product();
-    p.product();
+    p.product(1,2);
+   p.product(1,2,3);
+    p.product(3.45,4.5666);
   }
-class Product
+static class Product
 {
   public int product(int x, int y) {}
   public int product(int x, int y, int z) {}
