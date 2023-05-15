8a2af2564cfa5ab9c8ae69feca98ce7299b17139
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2023-05-15 17:14:50 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,7 +10,7 @@ public class Main {
   }
 
 }
- public static class Product
+ static class Product
 {
   public int product(int x, int y) { return x+y;}
   public int product(int x, int y, int z) { return x+y+z;}
